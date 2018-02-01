9e61873c35a1355449816336a1a14e900eb02259
RenGeng <geng2608@hotmail.com>
2018-02-01 22:21:45 +0100

    Ajout des panneaux

--- Production.py
+++ Production.py
@@ -21,19 +21,37 @@ class Production:
 
 class Eolienne(Production):
 	
-	def __init(self, identite):
-		self.id = i
+	def __init__(self, identite):
+		self.id = identite
 
 	def production(self):
 		self.energie= self.energie+1
 
 class PanneauPhotovoltaique(Production):
 	
-	def __init(self, identite):
-		self.id = i
+	def __init__(self, identite):
+		self.id = identite
+		self.nom="PanneauPhotovoltaique"
+		self.surface = 20.0 # En mètre carré
+		self.rendement = 0.14
+		self.radiation=0
+		
 
-	def production(self):
-		self.energie= self.energie+2
+	def set_radiation(self,temps):
+		file = open("radiation.txt", "r")
+		file.readline()
+		for line in file:
+			(time,G,Gd,Gc,DNI,DNIc,A,Ad,Ac)=line.split()
+			(heure,minute)=time.split(":")
+			if temps>=int(heure)*3600+int(minute)*60:
+				self.radiation=DNI
+		file.close()
+
+
+	def production(self,temps):
+		self.set_radiation(temps)
+		self.energie=float(self.rendement)*float(self.radiation)*float(self.surface)
+		return(self.energie)
 
 if __name__=='__main__':
 	p=Eolienne(1)
