02e210d7145e0df2e3618be239658a1cb83b6440
haroldGal <harold.gallice@gmail.com>
2018-03-07 16:44:43 +0100

    fin initialisation foyer

--- Tomaro_v2/Foyer.py
+++ Tomaro_v2/Foyer.py
@@ -10,12 +10,38 @@ from Personne import Personne
 class Foyer:
 
 	liste_personne = []
+	climatisation = False
+	chauffage = False
+	machine_a_laver = 0
+	lave_vaisselle = 0
+	seche_linge = 0
 
-	def __init__(self, _nombreIndividu):
-		self.nombre_individu = _nombreIndividu
-		self.ajouterIndividus()
+	def __init__(self, _nombre_individu):
 
-	def ChoixPopulation(self):
+		self.nombre_individu = _nombre_individu
+		self.ajouter_individu()
+
+
+	def habitude_foyer(self):
+		cpt_climatisation = 0
+		cpt_chauffage = 0
+
+		for individu in self.liste_personne:
+			self.machine_a_laver = individu.machine_a_laver if (self.machine_a_laver<individu.machine_a_laver) else self.machine_a_laver
+			self.lave_vaisselle = individu.lave_vaisselle if (self.lave_vaisselle<individu.lave_vaisselle) else self.lave_vaisselle
+			self.seche_linge = individu.seche_linge if (self.seche_linge<individu.seche_linge) else self.seche_linge
+			if(individu.climatisation):
+				cpt_climatisation+=1
+			if(individu.chauffage):
+				cpt_chauffage+=1
+
+		if(float(cpt_climatisation)/float(nombre_individu) > 0.5):	
+			climatisation = True
+		if(float(cpt_chauffage)/float(nombre_individu) > 0.5):
+			chauffage = True
+
+
+	def choix_population(self):
 		with open(LienSondage, 'rb') as FichierCsv:
 			lignes = csv.reader(FichierCsv, delimiter=',')
 			choix = sample(range(1,284),self.nombre_individu) # taille de la population on decale de 1 pouur header du csv
@@ -26,8 +52,8 @@ class Foyer:
 
 		return PopulationFoyer
 
-	def ajouterIndividus(self):
-		PopulationDisponible = self.ChoixPopulation()
+	def ajouter_individu(self):
+		PopulationDisponible = self.choix_population()
 		# print(PopulationDisponible)
 		for ligne in PopulationDisponible:
 			nouvel_individu = Personne()
@@ -48,7 +74,7 @@ class Foyer:
 					else:
 						nouvel_individu.pc_h_jt[360 + (index)*180]=int(var)
 
-
+			# Plaques à induction jours travaillés
 			for index, var in enumerate(ligne[14:21]):
 				if int(var)!=0:
 					if index==6:
@@ -56,6 +82,7 @@ class Foyer:
 					else:
 						nouvel_individu.pai_h_jt[360 + (index)*180]=int(var)
 
+			# Electro ménagers jours travaillés
 			for index, var in enumerate(ligne[21:28]):
 				if int(var)!=0:
 					if index==6:
@@ -63,6 +90,7 @@ class Foyer:
 					else:
 						nouvel_individu.electro_h_jt[360 + (index)*180]=int(var)
 
+			# Electro salle de bain jours travaillés
 			for index, var in enumerate(ligne[28:35]):
 				if int(var)!=0:
 					if index==6:
@@ -70,6 +98,7 @@ class Foyer:
 					else:
 						nouvel_individu.electro_h_jt[360 + (index)*180]=nouvel_individu.electro_h_jt[360 + (index)*180] + int(var)
 
+			# Television jours non travaillés
 			for index, var in enumerate(ligne[35:42]):
 				if int(var)!=0:
 					if index==6:
@@ -77,7 +106,7 @@ class Foyer:
 					else:
 						nouvel_individu.tv_h_jnt[360 + (index)*180]=int(var)
 
-			# Ordinateurs jours travaillés
+			# Ordinateurs jours non travaillés
 			for index, var in enumerate(ligne[42:49]):
 				if int(var)!=0:
 					if index==6:
@@ -85,7 +114,7 @@ class Foyer:
 					else:
 						nouvel_individu.pc_h_jnt[360 + (index)*180]=int(var)
 
-
+			# Plaques à induction jours non travaillés
 			for index, var in enumerate(ligne[49:56]):
 				if int(var)!=0:
 					if index==6:
@@ -93,6 +122,7 @@ class Foyer:
 					else:
 						nouvel_individu.pai_h_jnt[360 + (index)*180]=int(var)
 
+			# Electro ménager jours non travaillés
 			for index, var in enumerate(ligne[56:63]):
 				if int(var)!=0:
 					if index==6:
@@ -100,6 +130,7 @@ class Foyer:
 					else:
 						nouvel_individu.electro_h_jnt[360 + (index)*180]=int(var)
 
+			# Electro salle de bain jours non travaillés
 			for index, var in enumerate(ligne[63:70]):
 				if int(var)!=0:
 					if index==6:
@@ -120,7 +151,10 @@ class Foyer:
 			self.liste_personne.append(nouvel_individu)	
 
 
+
 if __name__=='__main__':
 	seed(3)
-	a = Foyer(5)
+	a = Foyer(1)
+	for i in a.liste_personne :
+		i.afficher()
 	#a.CsvParse()
